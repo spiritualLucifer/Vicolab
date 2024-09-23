311386a05956655dc2b995a0df88a597c2c47ba7
spiritualLucifer <virendryadav453@gmail.com>
2024-09-23 22:13:16 +0530

    h

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -14,7 +14,7 @@ function Home() {
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
         <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-          <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base'> Upcoming Meeting at : 12:30 </h2>
+          <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base'>Wellcome Back</h2>
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl font-extrabold lg:text-7xl'>
               {time}
